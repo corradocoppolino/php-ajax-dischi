c0718d464827b082bc158ff67870cd2453c34e25
corradocoppo <corradocoppolino@gmail.com>
2021-06-08 15:40:28 +0200

    init php

---
+++ index-php.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>PHP-DISCHI</title>
+</head>
+<body>
+
+    <?php include __DIR__ . '/data/db.php'; ?>
+    
+    <header>
+    
+        <i class="fab fa-spotify"></i>
+
+        <select id="genere">
+            <option value="rock">Rock</option>
+            <option value="indie">Indie</option>
+            <option value="rap">Rap</option>
+            <option value="trap" selected>Trap</option>
+        </select>
+
+    </header>
+
+    <main>
+
+        <div class="m-container">
+
+            <div class="box">
+
+                <img src="https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg" alt="">
+                <h3>New Jersey</h3>
+                <p>1998</p>
+
+            </div>
+
+            <div class="box">
+
+                <img src="https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg" alt="">
+                <h3>New Jersey</h3>
+                <p>1998</p>
+
+            </div>
+
+            <div class="box">
+
+                <img src="https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg" alt="">
+                <h3>New Jersey</h3>
+                <p>1998</p>
+
+            </div>
+
+            <div class="box">
+
+                <img src="https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg" alt="">
+                <h3>New Jersey</h3>
+                <p>1998</p>
+
+            </div>
+
+            <div class="box">
+
+                <img src="https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg" alt="">
+                <h3>New Jersey</h3>
+                <p>1998</p>
+
+            </div>
+
+            <div class="box">
+
+                <img src="https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg" alt="">
+                <h3>New Jersey</h3>
+                <p>1998</p>
+
+            </div>
+
+            <div class="box">
+
+                <img src="https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg" alt="">
+                <h3>New Jersey</h3>
+                <p>1998</p>
+
+            </div>
+
+            <div class="box">
+
+                <img src="https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg" alt="">
+                <h3>New Jersey</h3>
+                <p>1998</p>
+
+            </div>
+
+            <div class="box">
+
+                <img src="https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg" alt="">
+                <h3>New Jersey</h3>
+                <p>1998</p>
+
+            </div>
+
+            <div class="box">
+
+                <img src="https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg" alt="">
+                <h3>New Jersey</h3>
+                <p>1998</p>
+
+            </div>
+
+        </div>
+
+    </main>
+
+</body>
+</html>
