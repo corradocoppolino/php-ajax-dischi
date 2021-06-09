03ce083f1eb81755e01d2212f3bc62f499fd9a5a
corradocoppo <corradocoppolino@gmail.com>
2021-06-09 16:15:04 +0200

    finito da sistemare

--- api.php
+++ api.php
@@ -2,6 +2,35 @@
 
     include __DIR__ . '/data/db.php';
     header('Content-type: application/json');
-    echo json_encode($database);
+
+    $genres = [];
+    $albums = empty($_GET['genre']) || $_GET['genre'] === 'all' ? $database : [];
+
+    if(count($albums) === 0){
+
+        foreach($database as $album){
+            if($album['genre'] === $_GET['genre']){
+                $albums[] = $album;
+            }
+        }
+
+    }
+
+    foreach($database as $album){
+
+        if(!in_array($album['genre'],$genres)){
+            $genres[] = $album['genre'];
+        }
+
+    }
+
+    $response = [
+
+        'album' => $albums,
+        'genres' => $genres
+
+    ];
+
+    echo json_encode($response);
 
 ?>
--- script.js
+++ script.js
@@ -3,19 +3,42 @@ const app = new Vue({
     el: "#app",
 
     data:{
-        canzoni:[]
+        canzoni:[],
+        genere:[],
+        genereSearch: 'all',
+        apiURL: 'http://localhost:8888/php-ajax-dischi/api.php'
+    },
+
+    methods:{
+
+        getApi(){
+            axios.get(this.apiURL,{
+                params:{
+                    genre: this.genereSearch
+                }
+            }
+            )
+            .then(res => {
+
+                this.canzoni = res.data.album;
+                this.genere = res.data.genres;
+                console.log(this.canzoni);
+                console.log(this.genere);
+
+            })
+            .catch(err => {
+
+                console.log("error");
+                
+            })
+        }
+
     },
 
     created(){
-        axios.get('http://localhost:8888/php-ajax-dischi/api.php')
-        .then(res => {
-            console.log("ciaone")
-            this.canzoni = res.data;
-            console.log(this.dischi);
-        })
-        .catch(err => {
-            console.log("error");
-        })
+        
+        this.getApi();
+
     }
 
     
